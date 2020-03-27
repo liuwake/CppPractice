52369c580e5bf31ab3422aca4dff9baacd375fca
WK-HS <iwake@live.cn>
2020-03-27 17:00:27 +0800

    Start: CppDll and Archive Implicit linking

---
+++ Cpp/CppDll/DllTest/DllTest.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <windows.h>
+
+#pragma comment(lib,"CppDll.lib")
+extern "C" _declspec(dllimport) int add(int a, int b);
+extern "C" _declspec(dllimport) int sub(int a, int b);
+int main()
+{
+	int nParam1 = 9;
+	int nParam2 = 3;
+	int nAdd = add(nParam1, nParam2);
+	int nSub = sub(nParam1, nParam2);
+	std::cout << nAdd << ":" << nSub << std::endl;
+	system("pause");
+	return 0;
+}
---
+++ Cpp/CppDll/DllTest/DllTest.h
@@ -0,0 +1,5 @@
+//
+//#include "Dll1.h"  
+//#pragma comment(lib,"Dll1.lib")
+//
+////extern "C" __declspec(dllimport) int __stdcall add(int a, int b);
